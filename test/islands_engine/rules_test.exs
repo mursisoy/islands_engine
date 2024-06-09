906bbaa168903602592f5db349fc3e3ab1b8951e
Alberto Mur <mursisoy@proton.me>
2024-06-10 01:56:10 +0200

    Player 1 turn transition

--- test/islands_engine/rules_test.exs
+++ test/islands_engine/rules_test.exs
@@ -6,25 +6,23 @@ defmodule IslandsEngine.RulesTest do
   describe "check players can position islands" do
     setup do
       rules = Rules.new()
-      rules = %Rules{rules | state: :players_set}
+      {:ok, rules} = Rules.check(rules, :add_player)
       {:ok, %{rules: rules}}
     end
 
     test "player1 can position island", %{rules: rules} do
       assert Rules.check(rules, {:position_islands, :player1}) == {:ok, rules}
     end
-  end
-
-  describe "check players cannot position islands" do
-    setup do
-      rules = Rules.new()
-      rules = %Rules{rules | state: :players_set}
-      rules = %Rules{rules | player1: :islands_set}
-      {:ok, %{rules: rules}}
-    end
 
     test "player1 cannot position island", %{rules: rules} do
-      assert Rules.check(rules, {:position_islands, :player1}) == {:error}
+      {:ok, rules} = Rules.check(rules, {:set_islands, :player1})
+      assert Rules.check(rules, {:position_islands, :player1}) == :error
+    end
+
+    test "player1_turn transition", %{rules: rules} do
+      {:ok, rules} = Rules.check(rules, {:set_islands, :player1})
+      {:ok, rules} = Rules.check(rules, {:set_islands, :player2})
+      assert rules.state == :player1_turn
     end
   end
 end
